0c162bfb31867b65a7131dd2aa476d3d0fa72554
hobsRKM <yuvim86@gmail.com>
2021-09-07 22:48:02 +0530

    New Apis Added

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,13 +5,13 @@
 
 <!--Main layout-->
 <main style="margin-top: 86px">
+    @include("layout.breadcrum.breadcrum")
     @include($page, $data)
 </main>
 <!--Main layout-->
 <!-- MDB -->
 <script type="text/javascript" src="{{URL::to('/')}}/js/mdb.min.js"></script>
 <!-- Custom scripts -->
-<script type="text/javascript" src="{{URL::to('/')}}/js/admin.js"></script>
 <script src="{{URL::to('/')}}/js/socket/socket.js"></script>
 <script src="{{URL::to('/')}}/js/api/api.js"></script>
 </body>
