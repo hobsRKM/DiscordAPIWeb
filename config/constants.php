dc43e227509dddd364b2456132443be3af3bf778
hobsRKM <yuvim86@gmail.com>
2021-09-07 23:02:17 +0530

    New Apis Added

--- config/constants.php
+++ config/constants.php
@@ -2,4 +2,4 @@
 if (!defined('LAYOUT'))
     define("LAYOUT", "layout/layout");
 if (!defined('LAYOUT'))
-    define('SOCKET_URL','http://127.0.0.1:3025');
+    define('SOCKET_URL','http://127.0.0.1:3026');
